a95a8b166c27e50d3adb3a7d21a7f1ec6eeffed2
denebdev <deneb@chatzona.org>
2026-05-10 20:47:52 +0200

    Create clientinfo.h

---
+++ src/clientinfo.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "inspircd.h"
+
+struct ClientInfo
+{
+	std::string browser;
+	std::string os;
+	std::string device;
+	std::string engine;
+	std::string useragent;
+
+	bool mobile = false;
+	bool bot = false;
+	bool headless = false;
+	bool proxy = false;
+	bool secure = false;
+	bool malicious = false;
+
+	int riskscore = 0;
+
+	std::string riskreason;
+};
