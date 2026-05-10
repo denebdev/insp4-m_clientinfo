14f4ceea541e2b79c7e3780d492089e76841610d
denebdev <deneb@chatzona.org>
2026-05-10 19:22:26 +0200

    Update m_clientinfo.cpp

--- src/m_clientinfo.cpp
+++ src/m_clientinfo.cpp
@@ -25,6 +25,29 @@ class ModuleClientInfo : public Module
 	{
 	}
 
+	ModResult OnPreCommand(std::string& command,
+		CommandBase::Params& parameters,
+		LocalUser* user,
+		bool validated) override
+	{
+		if (command != "METADATA")
+			return MOD_RES_PASSTHRU;
+
+		if (parameters.size() < 3)
+			return MOD_RES_PASSTHRU;
+
+		if (parameters[1] != "useragent")
+			return MOD_RES_PASSTHRU;
+
+		ClientInfo ci;
+
+		DetectClientInfo(parameters[2], ci);
+
+		ext.Set(user, ci);
+
+		return MOD_RES_PASSTHRU;
+	}
+
 	Version GetVersion() override
 	{
 		return Version("Advanced WebIRC intelligence module", VF_VENDOR);
